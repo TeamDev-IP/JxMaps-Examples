3ba4ef925715b4583ffbe523ca1c14a2de12e075
Oleksandra Kovalenko <oleksandra.kovalenko@teamdev.com>
2016-05-06 15:19:05 +0300

    Add copyright header

--- src/com/teamdev/jxmaps/demo/ContentView.java
+++ src/com/teamdev/jxmaps/demo/ContentView.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2000-2016 TeamDev Ltd. All rights reserved.
+ * Use is subject to Apache 2.0 license terms.
+ */
 package com.teamdev.jxmaps.demo;
 
 import com.teamdev.jxmaps.examples.ControlPanel;
