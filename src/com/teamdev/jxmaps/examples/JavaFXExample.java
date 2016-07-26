40236e90e32f9c77ac29ba2a41a762c8170901c0
Vitaly Eremenko <vitaly.eremenko@teamdev.com>
2016-07-26 14:01:07 +0300

    JavaFXExample added

---
+++ src/com/teamdev/jxmaps/examples/JavaFXExample.java
@@ -0,0 +1,70 @@
+package com.teamdev.jxmaps.examples;
+
+/*
+ * Copyright (c) 2000-2016 TeamDev Ltd. All rights reserved.
+ * Use is subject to Apache 2.0 license terms.
+ */
+
+import com.teamdev.jxmaps.ControlPosition;
+import com.teamdev.jxmaps.LatLng;
+import com.teamdev.jxmaps.Map;
+import com.teamdev.jxmaps.MapOptions;
+import com.teamdev.jxmaps.MapReadyHandler;
+import com.teamdev.jxmaps.MapStatus;
+import com.teamdev.jxmaps.MapTypeControlOptions;
+import com.teamdev.jxmaps.javafx.MapView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+/**
+ * This example demonstrates how to create a MapView instance,
+ * display it in JavaFX application and open a simple map.
+ *
+ * @author Vitaly Eremenko
+ */
+public class JavaFXExample extends Application {
+
+    @Override
+    public void start(final Stage primaryStage) {
+        // Creation of a JavaFX map view
+        final MapView mapView = new MapView();
+
+        // Setting of a ready handler to MapView object. onMapReady will be called when map initialization is done and
+        // the map object is ready to use. Current implementation of onMapReady customizes the map object.
+        mapView.setOnMapReadyHandler(new MapReadyHandler() {
+            @Override
+            public void onMapReady(MapStatus status) {
+                // Check if the map is loaded correctly
+                if (status == MapStatus.MAP_STATUS_OK) {
+                    // Getting the associated map object
+                    final Map map = mapView.getMap();
+                    // Creating a map options object
+                    MapOptions options = new MapOptions();
+                    // Creating a map type control options object
+                    MapTypeControlOptions controlOptions = new MapTypeControlOptions();
+                    // Changing position of the map type control
+                    controlOptions.setPosition(ControlPosition.TOP_RIGHT);
+                    // Setting map type control options
+                    options.setMapTypeControlOptions(controlOptions);
+                    // Setting map options
+                    map.setOptions(options);
+                    // Setting the map center
+                    map.setCenter(new LatLng(35.91466, 10.312499));
+                    // Setting initial zoom value
+                    map.setZoom(2.0);
+                }
+            }
+        });
+
+        Scene scene = new Scene(new BorderPane(mapView), 700, 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
